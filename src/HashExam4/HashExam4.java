f1118d34edabd0595b43271ee86511cc0a86ae4b
Jaeguki <dev.jaeguki@gmail.com>
2019-01-21 20:21:34 +0900

    HashEaxm4...

---
+++ src/HashExam4/HashExam4.java
@@ -0,0 +1,93 @@
+/*
+베스트앨범
+문제 설명
+스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
+
+속한 노래가 많이 재생된 장르를 먼저 수록합니다.
+장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
+
+제한사항
+genres[i]는 고유번호가 i인 노래의 장르입니다.
+plays[i]는 고유번호가 i인 노래가 재생된 횟수입니다.
+genres와 plays의 길이는 같으며, 이는 1 이상 10,000 이하입니다.
+장르 종류는 100개 미만입니다.
+장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
+모든 장르는 재생된 횟수가 다릅니다.
+입출력 예
+genres	plays	return
+[classic, pop, classic, classic, pop]	[500, 600, 150, 800, 2500]	[4, 1, 3, 0]
+입출력 예 설명
+classic 장르는 1,450회 재생되었으며, classic 노래는 다음과 같습니다.
+
+고유 번호 3: 800회 재생
+고유 번호 0: 500회 재생
+고유 번호 2: 150회 재생
+pop 장르는 3,100회 재생되었으며, pop 노래는 다음과 같습니다.
+
+고유 번호 4: 2,500회 재생
+고유 번호 1: 600회 재생
+따라서 pop 장르의 [4, 1]번 노래를 먼저, classic 장르의 [3, 0]번 노래를 그다음에 수록합니다.
+*/
+package HashExam4;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+public class HashExam4 {
+	public int[] solution(String[] genres, int[] plays) {
+		
+		if (genres.length == 1)
+			return plays;
+		
+		int[] answer = {};
+		
+		//가장 많이 재생된 장르 선정
+		HashMap<String, Integer> hmTotCnt = new HashMap();
+
+		for(int i = 0 ; i < genres.length; i++) {
+			if(hmTotCnt.containsKey(genres[i])) {
+				hmTotCnt.replace(genres[i], hmTotCnt.get(genres[i])+plays[i]);
+			}else
+				hmTotCnt.put(genres[i], plays[i]);
+		}
+
+		Iterator<String> hmIterator = sortByValue(hmTotCnt).iterator();
+		List<String> genresRanking = new ArrayList<String>();
+		while(hmIterator.hasNext()){
+            String key = (String) hmIterator.next();
+            genresRanking.add(key);
+		}
+		
+		//장르 내에서 많이 재생된 노래 먼저 수록
+		
+		return answer;
+	}
+    public static List<String> sortByValue(final Map<String, Integer> map){
+        List<String> list = new ArrayList<String>();
+        list.addAll(map.keySet());
+         
+        Collections.sort(list,new Comparator<Object>(){
+            public int compare(Object o1,Object o2){
+                Object v1 = map.get(o1);
+                Object v2 = map.get(o2);
+                return ((Comparable<Object>) v1).compareTo(v2);
+            }
+             
+        });
+        Collections.reverse(list); // 주석시 오름차순
+        return list;
+    }
+
+	public static void main(String args[]) {
+		HashExam4 he = new HashExam4();
+		String[] genres = {"classic", "pop", "classic", "classic", "pop", "kpop"};
+		int[] plays = {500,600,150,800,2500,700};
+		he.solution(genres, plays);
+		
+	}
+}
